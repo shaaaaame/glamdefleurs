fe988e5003e4b47a3f5dba729964d26b7cf7fdcf
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2023-07-02 15:49:09 +0200

    edited routing

--- glamdefleurs_backend/glamdefleurs_api/flowers/urls.py
+++ glamdefleurs_backend/glamdefleurs_api/flowers/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from flowers import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'flowers', views.FlowerViewSet, basename="flower")
+router.register(r'categories', views.CategoryViewSet, basename="category")
+router.register(r'head_categories', views.HeadCategoryViewSet, basename="head_category")
+
 
 urlpatterns = [
-    path('', views.api_root),
-    path('flowers/', views.FlowerList.as_view()),
-    path('flowers/<int:pk>/', views.FlowerDetail.as_view()),
-    path('categories/', views.CategoryList.as_view()),
-    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
-    path('head_categories/', views.HeadCategoryList.as_view()),
-    path('head_categories/<int:pk>/', views.HeadCategoryDetail.as_view()),
+    path('', include(router.urls))
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
--- glamdefleurs_backend/glamdefleurs_api/flowers/views.py
+++ glamdefleurs_backend/glamdefleurs_api/flowers/views.py
@@ -1,43 +1,21 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from flowers.models import Flower, Category, HeadCategory
 from flowers.serializers import FlowerSerializer, CategorySerializer, HeadCategorySerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
-class FlowerList(generics.ListCreateAPIView):
-    """
-    List all code flowers, or create a new flower.
-    """
-    queryset = Flower.objects.all()
-    serializer_class = FlowerSerializer
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
-    
-class FlowerDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a flower.
-    """
+class FlowerViewSet(viewsets.ModelViewSet):
     queryset = Flower.objects.all()
     serializer_class = FlowerSerializer
     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
-class CategoryList(generics.ListCreateAPIView):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
-class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
-    
-class HeadCategoryList(generics.ListAPIView):
-    queryset = HeadCategory.objects.all()
-    serializer_class = HeadCategorySerializer
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
-
-
-class HeadCategoryDetail(generics.RetrieveAPIView):
+class HeadCategoryViewSet(viewsets.ModelViewSet):
     queryset = HeadCategory.objects.all()
     serializer_class = HeadCategorySerializer
     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
