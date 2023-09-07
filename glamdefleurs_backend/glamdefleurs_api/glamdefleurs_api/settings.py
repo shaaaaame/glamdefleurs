2eb16e952b08c6541cb997fbdd7107652da05f8f
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2023-09-07 16:57:17 -0400

    added static files

--- glamdefleurs_backend/glamdefleurs_api/glamdefleurs_api/settings.py
+++ glamdefleurs_backend/glamdefleurs_api/glamdefleurs_api/settings.py
@@ -166,3 +166,12 @@ AUTHENTICATION_BACKENDS = [
 CRONJOBS = [
     ('0 4 * * * *', 'flowers.cron.main')
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'whole-static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'whole-media')
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),
+]
