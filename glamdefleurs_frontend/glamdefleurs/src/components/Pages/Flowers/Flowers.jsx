4962cc6b4facdb97832115fc4c953f203ce5098a
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2023-11-03 16:22:50 -0400

    Revert "fixed bug where all adeded to full categories"
    This reverts commit 65c91675c05b7fb95dc693e24c6b8287d4f9e939.

--- glamdefleurs_frontend/glamdefleurs/src/components/Pages/Flowers/Flowers.jsx
+++ glamdefleurs_frontend/glamdefleurs/src/components/Pages/Flowers/Flowers.jsx
@@ -87,6 +87,17 @@ function Flowers() {
 
   // query for all categories
   const { data : fullCategories, isLoading: isFullLoading } = useQuery(['categories'], CategoryService.getCategories, { onSettled: (data) => {
+    // add "select" option
+    data.push({
+      id: "all",
+      name: "all",
+      subcategories: [{
+        id: "all",
+        name: "all",
+        head_category: "all"
+      }]
+    })
+
     setHead(data[fullCategories.length - 1].id);
     setSub(data[0].subcategories[0].id)
   }})
@@ -97,12 +108,8 @@ function Flowers() {
   const handleHeadChange = (e) => {
     setHead(e.target.value);
 
-    if (e.target.value === "all"){
-      setSub("all")
-    } else {
-      const headcategory = fullCategories.find(element => element.id === e.target.value)
-      setSub(headcategory.subcategories[0].id);
-    }
+    const headcategory = fullCategories.find(element => element.id === e.target.value)
+    setSub(headcategory.subcategories[0].id);
   }
 
 
@@ -117,21 +124,17 @@ function Flowers() {
           <div className='flowers-category-selectors-container'>
             <select className='flowers-head-category-selector' onChange={handleHeadChange} value={head}>
               {fullCategories.map(fc => <option value={fc.id}>{fc.name}</option>)}
-              <option value="all">all</option>
             </select>
             <select className='flowers-sub-category-selector' onChange={(e) => setSub(e.target.value)} value={sub}>
-              {
-                head === "all" ? <option value="all">all</option> :
-                  fullCategories.map(fc => {
-                    if(fc.id === head){
-                      const subOptions = fc.subcategories.map(sc => <option value={sc.id}>{sc.name}</option>)
-                      return subOptions
-                    }
-                    else{
-                      return
-                    }
-                  })
-              }
+              {fullCategories.map(fc => {
+                if(fc.id === head){
+                  const subOptions = fc.subcategories.map(sc => <option value={sc.id}>{sc.name}</option>)
+                  return subOptions
+                }
+                else{
+                  return
+                }
+              })}
             </select>
             <Link className='link flowers-category-selector-btn' to={sub === "all" ? '/categories/all' :  `/categories/s/${sub}` }><Filter /></Link>
           </div>
